d0f98c420e04d504cca43f9d5db8657e7b5e0792
daryl sangalang <dee.yess@live.com>
2017-09-17 19:55:32 +1000

    Initial Commit. New Models. New Context.

---
+++ SimpleShop/Context/SimpleShopDbContext.cs
@@ -0,0 +1,17 @@
+﻿using SimpleShop.Models;
+using System.Data.Entity;
+
+namespace SimpleShop.Context
+{
+    public class SimpleShopDbContext : DbContext
+    {
+        public SimpleShopDbContext() : base("SimpleShopContext")
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProdInfo> ProductInfo { get; set; }
+        //public DbSet<Shop> Shops { getl; set; }
+    }
+}
---
+++ SimpleShop/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleShop.Models
+{
+    public class Product
+    {
+    }
+}
